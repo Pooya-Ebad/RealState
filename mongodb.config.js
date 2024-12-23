42450fb01b98ce465f4cb556a7133a6d937955f9
POOYA <76815212+Pooya-Ebad@users.noreply.github.com>
2024-12-23 13:59:36 +0330

    Delete mongodb.config.js

--- mongodb.config.js
+++
@@ -1,9 +0,0 @@
-const { default: mongoose } = require("mongoose");
-const dotenv = require("dotenv")
-dotenv.config()
-
-mongoose.connect(process.env.MONGODB_URL).then(()=>{
-    console.log("connected to DB");
-}).catch((error)=>{
-    console.log(error?.message ?? "Failed DB connection");
-})
